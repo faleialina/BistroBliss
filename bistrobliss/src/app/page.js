cd913a82a6b58de34d194600202dd70366d77a2f
a.falei <august3@mail.ru>
2023-11-20 00:05:04 +0300

    HomePage/Footer

--- bistrobliss/src/app/page.js
+++ bistrobliss/src/app/page.js
@@ -9,15 +9,97 @@ export default function HomePage() {
           <h1>Best food for your taste</h1>
           <p>Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.</p>
         </div>
-        <div className={style.mainBtn}>
-          <button>Book A Table</button>
-          <button>Explore Menu</button>
+        <div className={style.mainBtns}>
+          <button className={style.btnTable}>Book A Table</button>
+          <button className={style.btnMenu}>Explore Menu</button>
         </div>
       </section>
 
-      <section className={style.blockmenu}></section>
-      
-      <section className={style.services}></section>
+      <section className={style.blockmenu}>
+        <div className={style.blockmenuTitle}>Browse Our Menu </div>
+
+        <div className={style.blockmenuItemsWrap}>
+
+          <div className={style.blockmenuItems}>
+            <div className={style.imgBreakfast}></div>
+            <div className={style.blockmenuText}>
+              <h1>Breakfast</h1>
+              <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+            </div>
+            <button className={style.btnBlockenu}>Explore Menu</button>
+          </div>
+
+          <div className={style.blockmenuItems}>
+            <div className={style.imgMainDishes}></div>
+            <div className={style.blockmenuText}>
+              <h1>Main Dishes</h1>
+              <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+            </div>
+            <button className={style.btnBlockenu}>Explore Menu</button>
+          </div>
+
+          <div className={style.blockmenuItems}>
+            <div className={style.imgDrinks}></div>
+            <div className={style.blockmenuText}>
+              <h1>Drinks</h1>
+              <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+            </div>
+            <button className={style.btnBlockenu}>Explore Menu</button>
+          </div>
+
+          <div className={style.blockmenuItems}>
+            <div className={style.imgDesserts}></div>
+            <div className={style.blockmenuText}>
+              <h1>Desserts</h1>
+              <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+            </div>
+            <button className={style.btnBlockenu}>Explore Menu</button>
+          </div>
+
+        </div>
+      </section>
+
+      <section className={style.services}>
+
+        <div className={style.servicesTitle}>We also offer unique services for your events </div>
+
+        <div className={style.servicesItemsWrap}>
+
+          <div className={style.servicesItems}>
+            <div className={style.imgCaterings}></div>
+            <div className={style.servicesText}>
+              <h1>Caterings</h1>
+              <p>In the new era of technology we look in the future with certainty for life.</p>
+            </div>
+          </div>
+
+          <div className={style.servicesItems}>
+            <div className={style.imgBirthdays}></div>
+            <div className={style.servicesText}>
+              <h1>Birthdays</h1>
+              <p>In the new era of technology we look in the future with certainty for life.</p>
+            </div>
+           </div>
+
+           <div className={style.servicesItems}>
+            <div className={style.imgWeddings}></div>
+            <div className={style.servicesText}>
+              <h1>Weddings</h1>
+              <p>In the new era of technology we look in the future with certainty for life.</p>
+            </div>
+            </div>
+
+            <div className={style.servicesItems}>
+            <div className={style.imgEvents}></div>
+            <div className={style.servicesText}>
+              <h1>Events</h1>
+              <p>In the new era of technology we look in the future with certainty for life.</p>
+            </div>
+            </div>
+
+        </div>
+
+      </section>
     </main>
   )
 }
--- bistrobliss/src/components/Footer/Footer.js
+++ bistrobliss/src/components/Footer/Footer.js
@@ -1,9 +1,46 @@
+import style from '../Footer/footer.module.css';
 
 export default function Footer() {
-    return (
-     <>
-     Footer
-     </>
-    )
-  }
-  
+  return (
+    <div className={style.footerWrap}>
+      <div className={style.col}>
+        <div className={style.col1}>
+          <div className={style.col1Title}>
+            <div className={style.imgCol1}></div>
+            <h1>Bistro Bliss</h1>
+          </div>
+          <p>In the new era of technology we look a in the future with certainty and pride to for our company and.</p>
+        </div>
+        <div className={style.col2}>
+          <div className={style.col2Text}>
+            <h1>Pages</h1>
+            <div className={style.elemNavig}>
+              {['Home', 'About', 'Menu', 'Pricing', 'Blog', 'Contact', 'Delivery'].map((el, index) => <p key={index}>{el}</p>)}
+            </div>
+          </div>
+          <div className={style.col2Text}>
+            <h1>Utility Pages</h1>
+            <div className={style.elemNavig}>
+              {['Start Here', 'Styleguide', 'Password Protected', '404 Not Found', 'Licenses', 'Changelog', 'View More'].map((el, index) => <p key={index}>{el}</p>)}
+            </div>
+          </div>
+        </div>
+        <div className={style.col3}>
+          {/* <div className={style.col3Text}> */}
+          <h1>Follow Us On Instagram</h1>
+          {/* </div> */}
+          <div className={style.imgsCol3}>
+            <div className={style.img1}></div>
+            <div className={style.img2}></div>
+            <div className={style.img3}></div>
+            <div className={style.img4}></div>
+          </div>
+
+        </div>
+      </div>
+      <div className={style.hashtag}>Copyright © 2023 Hashtag Developer. All Rights Reserved</div>
+
+    </div>
+
+  )
+}

3e714852184e40dda059a51ebee24d80db59dc1b
a.falei <august3@mail.ru>
2023-11-19 13:06:44 +0300

    sectionMain

--- bistrobliss/src/app/page.js
+++ bistrobliss/src/app/page.js
@@ -1,11 +1,22 @@
 
 import style from './page.module.css'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className={style.main}>
-      <section className={style.imgmain}></section>
+      <section className={style.imgmain}>
+        <div className={style.mainText}>
+          <h1>Best food for your taste</h1>
+          <p>Discover delectable cuisine and unforgettable moments in our welcoming, culinary haven.</p>
+        </div>
+        <div className={style.mainBtn}>
+          <button>Book A Table</button>
+          <button>Explore Menu</button>
+        </div>
+      </section>
+
       <section className={style.blockmenu}></section>
+      
       <section className={style.services}></section>
     </main>
   )

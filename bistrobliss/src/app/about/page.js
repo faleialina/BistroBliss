f7b43d5e3028dc103f0dbd9cf790b1f4e3162061
a.falei <august3@mail.ru>
2023-11-21 21:30:49 +0300

    About

---
+++ bistrobliss/src/app/about/page.js
@@ -0,0 +1,46 @@
+import style from '../about/about.module.css'
+export default function About() {
+    return (
+        <main>
+            <section className={style.mainAbout}>
+                <div className={style.wrap}>
+                    <div className={style.imgAbout}></div>
+                    <div className={style.aboutText}>
+                        <h1>We provide healthy food for your family.</h1>
+                        <h2>Our story began with a vision to create a unique dining experience that merges fine dining, exceptional service, and a vibrant ambiance. Rooted in city's rich culinary culture, we aim to honor our local roots while infusing a global palate.</h2>
+                        <p>At place, we believe that dining is not just about food, but also about the overall experience. Our staff, renowned for their warmth and dedication, strives to make every visit an unforgettable event.</p>
+                    </div>
+                </div>
+            </section>
+            <section className={style.statistic}>
+                <div className={style.wrapStatistic}>
+                    <div className={style.aboutText}>
+                        <h1>A little information for our valuable guest</h1>
+                        <p>At place, we believe that dining is not just about food, but also about the overall experience. Our staff, renowned for their warmth and dedication, strives to make every visit an unforgettable event.</p>
+                    </div>
+
+                    <div className={style.statisticBlock}>
+                        <div className={style.item_1}>
+                            <h2>3</h2>
+                            <p>Locations</p>
+                        </div>
+                        <div className={style.item_2}>
+                            <h2>1995</h2>
+                            <p>Founded</p>
+                        </div>
+                        <div className={style.item_3}>
+                            <h2>65+</h2>
+                            <p>Staff Members</p>
+                        </div>
+                        <div className={style.item_4}>
+                            <h2>100%</h2>
+                            <p>Satisfied Customers</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.imgStatistic}></div>
+
+            </section>
+        </main>
+    )
+}
--- bistrobliss/src/components/Header/Header.js
+++ bistrobliss/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import style from '../Header/header.module.css';
 export default function Header() {
   return (
@@ -6,12 +7,11 @@ export default function Header() {
         <div className={style.phone}>
           <div className={style.imgPhone}></div>
           <p>(414) 857 - 0107</p>
-        </div>
-
-        <div className={style.mail}>
+           <div className={style.mail}>
           <div className={style.imgMail}></div>
           <p>yummy@bistrobliss</p>
         </div>
+        </div>   
 
         <div className={style.images}>
           <div className={style.imgTwitter}></div>
@@ -25,7 +25,11 @@ export default function Header() {
         <div className={style.logo}></div>
         <h1>Bistro Bliss</h1>
         <div className={style.elemNavig}>
-          {['Home', 'About', 'Menu'].map((el, index) => <p key={index}>{el}</p>)}
+          {/* {['home', 'about', 'menu'].map((el, index) => <p key={index}><Link href={el}>{el}</Link></p>)} */}
+          <p><Link href='/'>home</Link></p>
+          <p><Link href='/about'>about</Link></p>
+          <p><Link href='/menu'>menu</Link></p>
+
         </div>
 
         <button className={style.btn}>Book A Table</button>

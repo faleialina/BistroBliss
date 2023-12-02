55f80969f79c5ecfd037ad3c50d8fc5d0b9ab14d
a.falei <august3@mail.ru>
2023-12-02 23:47:41 +0300

    dynamicRouterStyle

--- bistrobliss/src/app/menu/[id]/page.js
+++ bistrobliss/src/app/menu/[id]/page.js
@@ -1,6 +1,6 @@
 'use client'
 import { useParams } from 'next/navigation';
-// import style from './menu.module.css';
+import style from './id.module.css';
 import { useEffect, useState } from 'react';
 import storage from '../../../storage/storage'
 import Image from 'next/image';
@@ -15,11 +15,17 @@ export default function Id() {
   }, [id])
 
   return (
-    <>
-      <div><Image src={data?.img} /></div>
-      <h1>{data?.title}</h1>
-      <h2>{data?.description}</h2>
-      <p>Total:{data?.price}</p>
-    </>
+    <div className={style.wrapper}>
+      <div className={style.wrapperItem}>
+        
+          <Image className={style.imgItem} src={data?.img} />
+      
+        <div className={style.textItem}>
+          <h1>{data?.title}</h1>
+          <h2>{data?.description}</h2>
+          <p>Total:{data?.price}</p>
+        </div>
+      </div>
+    </div>
   )
 }
